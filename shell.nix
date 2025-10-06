270749d0bd20728f8069b0fcd1c5738d6f0e46e5
EinSatzMitX <EinSatzMitX@users.noreply.github.com>
2025-10-06 22:27:35 +0200

    yay, it compiles (and runs) on nixos :D

--- shell.nix
+++ shell.nix
@@ -1,18 +1,22 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  # Get dependencies from the main package
-  inputsFrom = [(pkgs.callPackage ./default.nix {})];
-  # Additional tooling
-  nativeBuildInputs = with pkgs; [
-    pkg-config
+{pkgs ? import <nixpkgs> {}}: let
+  lib = pkgs.lib;
+  libPath = lib.makeLibraryPath [
+    pkgs.libGL
+    pkgs.libxkbcommon
+    pkgs.wayland
   ];
+in
+  with pkgs;
+    mkShell {
+      buildInputs = [
+        cargo
+        rustc
+        rust-analyzer
+      ];
 
-  buildInputs = with pkgs; [
-    rust-analyzer # LSP Server
-    rustfmt # Formatter
-    clippy # Linter
-    wayland
-    wlr-protocols
-    libxkbcommon
-  ];
-}
+      shellHook = ''
+        export LD_LIBRARY_PATH=${libPath}:$LD_LIBRARY_PATH
+        export RUST_LOG=debug
+        export RUST_SRC_PATH=${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}
+      '';
+    }
