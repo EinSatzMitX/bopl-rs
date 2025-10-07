4dae3a3caf32e6de2946097982ff68665ad63c87
EinSatzMitX <EinSatzMitX@users.noreply.github.com>
2025-10-07 21:12:24 +0200

    added desktop file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,88 @@
+use std::process::Command;
+
 use winit::{
     application::ApplicationHandler,
-    dpi::LogicalSize,
+    // dpi::LogicalSize,
     event::WindowEvent,
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
+
+#[derive(Debug)]
+struct DEField<'a> {
+    key: &'a str,
+    value: &'a str,
+}
+
+#[derive(Debug)]
+struct DEGroup<'a> {
+    name: &'a str,
+    members: Vec<DEField<'a>>,
+}
+
+#[derive(Debug)]
+struct DesktopEntry<'a> {
+    groups: Vec<DEGroup<'a>>,
+}
+
+impl<'a> DesktopEntry<'a> {
+    fn from_str(contents: &'a str) -> DesktopEntry<'a> {
+        // Start parsing here
+        let mut groups: Vec<DEGroup<'a>> = Vec::new();
+        let mut current_group: Option<DEGroup<'a>> = None;
+
+        for (line_no, raw_line) in contents.lines().enumerate() {
+            let line = raw_line.trim();
+
+            /* Skip empty or commented lines (Even `  # stuff` counts as commented, because of the
+             * tirm function) */
+            if line.is_empty() || line.starts_with('#') {
+                continue;
+            }
+
+            /* Group parsing */
+            if line.starts_with('[') && line.ends_with(']') {
+                if let Some(g) = current_group.take() {
+                    groups.push(g);
+                }
+                let name = line[1..line.len() - 1].trim();
+                current_group = Some(DEGroup {
+                    name,
+                    members: Vec::new(),
+                });
+                continue;
+            }
+
+            if let Some((k, v)) = line.split_once('=') {
+                let key = k.trim();
+                let value = v.trim();
+
+                if let Some(g) = current_group.as_mut() {
+                    g.members.push(DEField { key, value });
+                } else {
+                    /* Key-value-pair before any groups, I don't know if this is allowed but let's
+                     * handle it anyways */
+                    current_group = Some(DEGroup {
+                        name: "",
+                        members: vec![DEField { key, value }],
+                    });
+                }
+            } else {
+                eprintln!(
+                    "Warning: ignoring malformed line {}: {:?}",
+                    line_no + 1,
+                    raw_line
+                )
+            }
+        }
+
+        if let Some(g) = current_group {
+            groups.push(g);
+        }
+
+        DesktopEntry { groups }
+    }
+}
 
 #[derive(Default)]
 struct App {
@@ -46,6 +124,34 @@ fn main() {
 
     event_loop.set_control_flow(ControlFlow::Wait);
 
+    let output = Command::new("yazi")
+        // .arg("")
+        .output()
+        .expect("Failed to execute command");
+
     let mut app = App::default();
     let _ = event_loop.run_app(&mut app);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::DesktopEntry;
+
+    #[test]
+    fn parse_test() {
+        let sample = r#"
+# example .desktop
+[Desktop Entry]
+Name=Hello Triangle
+Comment=Shows a triangle
+Exec=hello_triangle
+Type=Application
+
+[Hidden]
+Hidden=true
+"#;
+
+        let de = DesktopEntry::from_str(sample);
+        println!("{:?}", de);
+    }
 }
